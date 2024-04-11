28568fe3e12a278e2f3b2315b59ea3b8633b6049
zinwailwin <zinwailwin395@gamil.com>
2024-04-11 10:23:55 +0630

    Complete Ado.Net CRUD

--- ZWLDotNetCore.ConsoleApp/Program.cs
+++ ZWLDotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Data;
+using System.Data.SqlClient;
+using ZWLDotNetCore.ConsoleApp;
+
 Console.WriteLine("Hello, World!");
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create("Biography", "Dr. Ye Thu Wai", "This is the history book");
+//adoDotNetExample.Update(1003, "Test Title","Test Author", "TEst Content");
+//adoDotNetExample.Delete(1003);
+//adoDotNetExample.Edit(1002);
+adoDotNetExample.Edit(1003);
+Console.ReadKey();
