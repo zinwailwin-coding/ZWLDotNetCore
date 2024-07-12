9f749dd38a728f6dc37fd0eef32a00e8a88a2d97
zinwailwin <zinwailwin395@gamil.com>
2024-07-12 10:34:23 +0630

    [Complete] HighChart

---
+++ ZWLDotNetCore.MvcChartApp/Controllers/HighChartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZWLDotNetCore.MvcChartApp.Controllers
+{
+    public class HighChartController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+    }
+}
