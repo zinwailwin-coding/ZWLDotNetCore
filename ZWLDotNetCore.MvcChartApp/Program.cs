3d0112fb51283e06be0327886a05c4f573d2e5f9
zinwailwin <zinwailwin395@gamil.com>
2024-07-12 13:20:26 +0630

    [Complete] DbLog

--- ZWLDotNetCore.MvcChartApp/Program.cs
+++ ZWLDotNetCore.MvcChartApp/Program.cs
@@ -1,8 +1,13 @@
 using Serilog;
+using Serilog.Sinks.MSSqlServer;
 string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs/ZWLDotNetCore.MvcChartApp.log");
 Log.Logger = new LoggerConfiguration()
 	.WriteTo.Console()
 	.WriteTo.File(filePath, rollingInterval: RollingInterval.Hour)
+	.WriteTo
+	 .MSSqlServer(
+		connectionString: "Server=DHO-LP-23-006\\SQLSERVER2022;Database=DotNetTrainging4;User ID=sa;Password=sa@123;TrustServerCertificate=true",
+		sinkOptions: new MSSqlServerSinkOptions { TableName = "Tbl_LogEvents", AutoCreateSqlTable=true })
 	.CreateLogger();
 try
 {
