f7c67b49c4a8149c4b81923ee3eabd58bb7f7846
zinwailwin <zinwailwin395@gamil.com>
2024-07-12 10:47:16 +0630

    [Complete] CanvasJs

---
+++ ZWLDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZWLDotNetCore.MvcChartApp.Controllers
+{
+    public class CanvasJsController : Controller
+    {
+        public IActionResult LineChart()
+        {
+            return View();
+        }
+    }
+}
