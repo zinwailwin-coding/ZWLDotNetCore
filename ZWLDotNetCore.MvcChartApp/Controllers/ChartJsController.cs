c92dcb345b12a0147df892de261b2d9d0cbf48a7
zinwailwin <zinwailwin395@gamil.com>
2024-07-12 10:19:21 +0630

    [Complete] ChartJs

---
+++ ZWLDotNetCore.MvcChartApp/Controllers/ChartJsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZWLDotNetCore.MvcChartApp.Controllers
+{
+    public class ChartJsController : Controller
+    {
+        public IActionResult ExampleChart()
+        {
+            return View();
+        }
+        public IActionResult InterpolationLineChart()
+        {
+            return View();
+        }
+    }
+}
